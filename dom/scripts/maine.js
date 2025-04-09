c837976d8362ff021f9f40cb0d2c16ce79b7afa3
DLevi04 <doboczi.levente@gmail.com>
2025-04-09 17:26:44 +0200

    Update(JavaScript)

---
+++ dom/scripts/maine.js
@@ -0,0 +1,21 @@
+const btnGroup = document.querySelector('.btn-group');
+const prevBtn = btnGroup.querySelector('button.btn:nth-child(1)');
+const numBtn = btnGroup.querySelector('button.btn:nth-child(2)');
+const nextBtn = btnGroup.querySelector('button.btn:nth-child(3)');
+if (prevBtn) {
+    prevBtn.classList.add('btn-info'); //info osztály hozzáadása a gomb osztályához
+}
+
+if(numBtn) {
+    numBtn.classList.add('btn-primary'); 
+}
+
+if(nextBtn) {
+    nextBtn.classList.add('btn-info');
+}
+
+prevBtn.addEventListener('click', (e) => {
+    console.log(e);
+    
+});
+
